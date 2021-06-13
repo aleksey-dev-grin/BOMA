233c2cfd74fa61d21a7bbfcc2eab2d74c14b5542
Aleksey Grin <iamdasuper@gmail.com>
2021-06-13 16:41:41 +0300

    New Module: 4 - Color Descriptor - Spinner - Button - TextView

--- 4_colordescriptor/src/main/java/com/example/a4_colordescriptor/MainActivity.java
+++ 4_colordescriptor/src/main/java/com/example/a4_colordescriptor/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.a4_colordescriptor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private Button button;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+        tv = findViewById(R.id.tv);
+
     }
+
+
+    public void showDescription(View view) {
+        int position = spinner.getSelectedItemPosition();
+        tv.setText(getDecription(position));
+    }
+
+    private String getDecription(int position) {
+        String[] descriptions = getResources().getStringArray(R.array.descript_of_temp);
+        return descriptions[position];
+    }
+
+
 }
