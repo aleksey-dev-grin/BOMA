03867a7f6d5dca1f915d8484ca02e70f7021e3a6
Aleksey Grin <iamdasuper@gmail.com>
2021-06-13 19:17:20 +0300

    New Module: 5 - Many Activitys -add EditText -add Button -add method on Button -add Activity for massage

--- 5_manyactivities/src/main/java/com/example/a5_manyactivities/MainActivity.java
+++ 5_manyactivities/src/main/java/com/example/a5_manyactivities/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.a5_manyactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextMsg = findViewById(R.id.editTextMsg);
     }
+
+    public void onClickSendMsg(View view) {
+        String msg = editTextMsg.getText().toString();
+    }
+
 }
---
+++ 5_manyactivities/src/main/java/com/example/a5_manyactivities/RecievedMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.a5_manyactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecievedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieved_message);
+    }
+}
