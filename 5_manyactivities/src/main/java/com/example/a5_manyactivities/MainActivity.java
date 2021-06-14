a8694fdae3b248c7c9e30c8b2a73b7a4d2cd6129
Aleksey Grin <iamdasuper@gmail.com>
2021-06-14 11:37:45 +0300

    New Module: 5 - Many Activitys -add Intent

--- 5_manyactivities/src/main/java/com/example/a5_manyactivities/MainActivity.java
+++ 5_manyactivities/src/main/java/com/example/a5_manyactivities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a5_manyactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickSendMsg(View view) {
+        Intent intent = new Intent(this, RecievedMessageActivity.class);
         String msg = editTextMsg.getText().toString();
+        startActivity(intent.putExtra("message", msg));
+
     }
 
 }
--- 5_manyactivities/src/main/java/com/example/a5_manyactivities/RecievedMessageActivity.java
+++ 5_manyactivities/src/main/java/com/example/a5_manyactivities/RecievedMessageActivity.java
@@ -3,12 +3,20 @@ package com.example.a5_manyactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecievedMessageActivity extends AppCompatActivity {
+
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recieved_message);
+
+        Bundle args = getIntent().getExtras();
+        String msg = args.getString("message").toString();
+        tvResult = findViewById(R.id.tvResult);
+        tvResult.setText(msg);
     }
 }
